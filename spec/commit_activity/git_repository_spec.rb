0a3a75af5f95baf9532806c9d790813c0ebd7b80
monochromegane <dev.kuro.obi@gmail.com>
2013-11-20 23:18:56 +0900

    Modify spec because GitRepository requires url when it initialize.

--- spec/commit_activity/git_repository_spec.rb
+++ spec/commit_activity/git_repository_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 require 'commit_activity/git_repository'
 
 describe CommitActivity::GitRepository do
-  subject { CommitActivity::GitRepository.new }
+  let(:url) { 'url' }
+  subject { CommitActivity::GitRepository.new(url) }
   it { should be_instance_of CommitActivity::GitRepository }
 end
 
