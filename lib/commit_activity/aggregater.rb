3815e52000401dbbde21c8b497b94a9c22bf25d7
monochromegane <dev.kuro.obi@gmail.com>
2013-11-20 23:40:41 +0900

    Supports method chain.

--- lib/commit_activity/aggregater.rb
+++ lib/commit_activity/aggregater.rb
@@ -1,11 +1,16 @@
 module CommitActivity
   class Aggregater
-    attr_accessor :repositories
-    def aggregate(*repositories)
-      @repositories = []
-      repositories.each {|repo| @repositories << CommitActivity::GitRepository.new(repo) }
+
+    def on(*repos)
+      repos.each {|repo| repositories << CommitActivity::GitRepository.new(repo) }
+      self
     end
+
     def since
+    end
+
+    def repositories
+      @repositories ||= []
     end
   end
 end
--- spec/commit_activity/aggregater_spec.rb
+++ spec/commit_activity/aggregater_spec.rb
@@ -5,21 +5,23 @@ require 'commit_activity/git_repository'
 describe CommitActivity::Aggregater do
   subject { CommitActivity::Aggregater.new }
   it { should be_instance_of CommitActivity::Aggregater }
-  it { should be_respond_to :aggregate }
+  it { should be_respond_to :on }
   it { should be_respond_to :since }
 
-  describe 'aggregate' do
+  describe 'on' do
 
     context 'when a repository given.' do
       let(:params) { 'repository_name' }
-      before { subject.aggregate(params) }
+      subject { CommitActivity::Aggregater.new.on(params) }
+      it { should be_instance_of CommitActivity::Aggregater }
       it { expect(subject.repositories.first).to be_instance_of CommitActivity::GitRepository }
       it { expect(subject.repositories.first.url).to be_eql params }
     end
 
     context 'when repositories given.' do
       let(:params) { ['repository_a', 'repository_b'] }
-      before { subject.aggregate(*(params)) }
+      subject { CommitActivity::Aggregater.new.on(*(params)) }
+      it { should be_instance_of CommitActivity::Aggregater }
       it { expect(subject.repositories.first).to be_instance_of CommitActivity::GitRepository }
       it { expect(subject.repositories.first.url).to be_eql params.first }
     end
