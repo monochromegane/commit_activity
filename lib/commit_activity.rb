8096942f7dd57d8008d7df01a926e8a9105deab0
monochromegane <dev.kuro.obi@gmail.com>
2013-11-25 00:16:11 +0900

    Added configuration to commit_activity.

--- lib/commit_activity.rb
+++ lib/commit_activity.rb
@@ -3,8 +3,19 @@ require 'commit_activity/aggregater'
 
 module CommitActivity
   class << self
+    attr_accessor :configuration
+
+    def configure
+      self.configuration ||= Configuration.new
+      yield configuration
+    end
+
     def aggregate
       CommitActivity::Aggregater.new
     end
+  end
+
+  class Configuration
+    attr_accessor :repositories
   end
 end
--- spec/commit_activity_spec.rb
+++ spec/commit_activity_spec.rb
@@ -5,6 +5,19 @@ describe CommitActivity do
     CommitActivity::VERSION.should_not be_nil
   end
 
+  describe 'configure' do
+    let(:repos) { ['repo1'] }
+    subject { CommitActivity.configuration }
+    context 'when configure' do
+      before do
+        CommitActivity.configure do |config|
+          config.repositories = repos
+        end
+      end
+      its(:repositories) { should be_eql repos }
+    end
+  end
+
   describe 'aggregate' do
     subject { CommitActivity.aggregate }
     it { should be_instance_of CommitActivity::Aggregater }
